4068b5a0cab9f53095668a09cee8951e3dd1bf01
Dario Di Gulio <ddigulio@technisys.com>
2020-04-05 17:57:07 -0300

    upgrade 1/3

---
+++ Estelarte/index.js
@@ -0,0 +1,341 @@
+const viejo = {
+  id: "viejo",
+  url: "url('imagenes/viejo.png')",
+  title: "Don José",
+  description: "La vejez empieza cuando perdemos la curiosidad.",
+};
+
+const bandera = {
+  id: "bandera",
+  url: "url('imagenes/bandera.png')",
+  title: "Argentina",
+  description: "Cielo, azul y cielo.",
+};
+
+const elefante = {
+  id: "elefante",
+  url: "url('imagenes/elefante.png')",
+  title: "Tembo",
+  description: "Mi segunda obra año 2005.",
+};
+
+const tango = {
+  id: "tango",
+  url: "url('imagenes/tango.png')",
+  title: "Caminito",
+  description: "Obra donada.",
+};
+
+const buho = {
+  id: "buho",
+  url: "url('imagenes/buho.png')",
+  title: "Al asecho",
+  description: "Espera calma y sigilosa.",
+};
+
+const aguila = {
+  id: "aguila",
+  url: "url('imagenes/aguila.png')",
+  title: "El Vuelo",
+  description:
+    "Porque ir andando, si puedes volar. Vuela hasta volvernos a encontrar.",
+};
+
+const paisaje = {
+  id: "paisaje",
+  url: "url('imagenes/paisaje.png')",
+  title: "Desde el faro",
+  description: "Cuanto más te miro más te creo.",
+};
+
+const avenida = {
+  id: "avenida",
+  url: "url('imagenes/avenida.png')",
+  title: "Buenos Aires",
+  description: "Las luces de mi ciudad, esperan siempre allí.",
+};
+
+const uruguay = {
+  id: "uruguay",
+  url: "url('imagenes/uruguay.png')",
+  title: "Silencio",
+  description:
+    "Historias de pinceles. Viajando por las playas de Uruguay el paisaje me encontró, y entonces se lo conté al lienzo que esperaba teñirse de color.",
+};
+
+const madre = {
+  id: "madre",
+  url: "url('imagenes/madre.png')",
+  title: "Desde el Alma",
+  description:
+    "No hay cielo tan puro como tu voz, no hay mar tan profundo como tu mirada y no hay sabio tan sabio que me enseñe lo que has venido a enseñarme tu hijo mío. Gracias a D.DG.",
+};
+
+const caballo = {
+  id: "caballo",
+  url: "url('imagenes/caballo.png')",
+  title: "Lucero",
+  description:
+    "Aunque el frio queme, aunque el miedo muerda, aunque el sol se esconda.",
+};
+
+const louis = {
+  id: "louis",
+  url: "url('imagenes/louis.png')",
+  title: "Mundo maravilloso",
+  description:
+    "Veo cielos de color azul, y nubes de color blanco. El brillante bendecido día, la oscura sagrada noche, y pienso para mí... qué mundo tan maravilloso.",
+};
+
+const tigre = {
+  id: "tigre",
+  url: "url('imagenes/tigre.png')",
+  title: "Miradas",
+  description:
+    "Historias de pinceles : Un día alguien me dijo: 'Me gustaría ver esta imagen pintada en grande para poderla contemplar día tras dia'. Así es como surgió Miradas.",
+};
+
+const estela = {
+  id: "estela",
+  url: "url('imagenes/estela.jpg')",
+  title: "Miradas",
+  description:
+    "Técnica: arte figurativo con acrilico. Medias: 50x80. Año: 2018",
+};
+
+const mane = {
+  id: "mane",
+  url: "url('imagenes/marianela.jpg')",
+  title: "Marianela",
+  description: "Obra expuesta en el Carrusel del Louvre año 2019",
+};
+
+const jazz = {
+  id: "jazz",
+  url: "url('imagenes/jazz.jpg')",
+  title: "Jazz",
+  description: "En homenaje a mis queridos amigos Julieta y Mario.",
+};
+
+const patines = {
+  id: "patines",
+  url: "url('imagenes/patines.jpg')",
+  title: "Soñando sobre ruedas",
+  description: "tec acrilico sobre carton entelado medidas 45x60 año 2006",
+};
+
+const anju = {
+  id: "anju",
+  url: "url('imagenes/anju.png')",
+  title: "Anju",
+  description: "No busques con los ojos lo que puedes ver con el corazón.",
+};
+
+const abstract1 = {
+  id: "abstract1",
+  url: "url('imagenes/abstract1.png')",
+  title: "Abstracto",
+  description: "Abstracto con textura.",
+};
+
+const crisol = {
+  id: "crisol",
+  url: "url('imagenes/crisol.png')",
+  title: "Crisol",
+  description: "Un lugar, un recuerdo en el tiempo.",
+};
+
+const paleta = {
+  id: "paleta",
+  url: "url('imagenes/abstract2.png')",
+  title: "Desde la pleta",
+  description: "Técnica abstracto con acrilico medidas 80x80 año 2018",
+};
+
+const abstract3 = {
+  id: "abstract3",
+  url: "url('imagenes/abstract3.png')",
+  title: "Abstracto",
+  description: "2019",
+};
+
+const infinito = {
+  id: "infinito",
+  url: "url('imagenes/infinito.jpg')",
+  title: "Abstracto",
+  description: "¿Sabías que en la cultura quechua el color azul representa el infinito? (araxa- pacha).",
+};
+
+const paris = {
+  id: "paris",
+  url: "url('imagenes/paris.jpg')",
+  title: "París",
+  description: "Del caos a la calma . Obra escencialista",
+};
+
+const renacer = {
+  id: "renacer",
+  url: "url('imagenes/renacer.png')",
+  title: "Renacer",
+  description: "tec arte figurat con acrilico medidas 60x60 año 2016",
+};
+
+const refugio = {
+  id: "refugio",
+  url: "url('imagenes/refugio.png')",
+  title: "El refugio",
+  description: "tec oleo con pincel seco en madera medidas 60x75 año 2005",
+};
+
+const pateles1 = {
+  id: "pasteles1",
+  url: "url('imagenes/pasteles1.png')",
+  title: "Ensueño I",
+  description: "Serie de cuadros pintados con pasteles.",
+};
+
+const pateles2 = {
+  id: "pasteles2",
+  url: "url('imagenes/pasteles2.png')",
+  title: "Ensueño II",
+  description: "Serie de cuadros pintados con pasteles.",
+};
+
+const expo1 = {
+  id: "expo1",
+  url: "url('imagenes/expo1.png')",
+  title: "Exposición en Buenos Aires",
+  description: "2014",
+};
+
+const expo2 = {
+  id: "expo2",
+  url: "url('imagenes/expo2.png')",
+  title: "Exposición en París",
+  description: "2019",
+};
+
+const calas = {
+  id: "calas",
+  url: "url('imagenes/calas.png')",
+  title: "Calas",
+  description: "No dejes que le de sed al árbol del que eres sol. FK.",
+};
+
+const dataCuadros = [
+  viejo,
+  bandera,
+  elefante,
+  tango,
+  aguila,
+  buho,
+  paisaje,
+  avenida,
+  uruguay,
+  madre,
+  caballo,
+  louis,
+  tigre,
+  estela,
+  mane,
+  jazz,
+  patines,
+  anju,
+  abstract1,
+  crisol,
+  paleta,
+  abstract3,
+  infinito,
+  paris,
+  renacer,
+  refugio,
+  pateles1,
+  pateles2,
+  expo1,
+  expo2,
+  calas,
+];
+
+function cargarModal(cuadro) {
+  let dataCuadro = bucarDataCuadro(cuadro);
+  document.getElementById("title_modal").innerHTML = dataCuadro.title;
+  document.getElementById("description_modal").innerHTML =
+    dataCuadro.description;
+  document.getElementById("image_modal").style.backgroundImage = dataCuadro.url;
+  // UIkit.modal("#modal_viejo").toggle();
+}
+
+function bucarDataCuadro(idCuadro) {
+  for (const cuadro of dataCuadros) {
+    if (cuadro.id == idCuadro) {
+      return cuadro;
+    }
+  }
+}
+
+// function crearModal() {
+//   let divPadre = crearDivPadre();
+//   let divHijo = crearDivHijo();
+//   let botonClose = crearBotonClose();
+//   let divInterno = crearDivInterno();
+//   let divImage = crearDivImage();
+//   let divDescription = crearDivDescription();
+//   divPadre.appendChild(divHijo);
+//   divPadre.appendChild(botonClose);
+//   divPadre.appendChild(divInterno);
+//   divInterno.appendChild(divImage);
+//   divInterno.appendChild(divDescription);
+//   var body = document.getElementsByTagName("body")[0]
+//   body.appendChild(divPadre);
+//   console.log(body);
+
+// }
+
+function crearDivPadre() {
+  let div = document.createElement("div");
+  div.id = "modal_viejo";
+  div.className = "uk-modal-ful";
+  div.setAttribute("uk-modal", "");
+  return div;
+}
+
+function crearDivHijo() {
+  let div = document.createElement("div");
+  div.className = "uk-modal-dialog";
+  return div;
+}
+
+function crearBotonClose() {
+  let button = document.createElement("button");
+  button.className = "uk-modal-close-default";
+  button.type = "button";
+  button.setAttribute("uk-close", "");
+  return button;
+}
+
+function crearDivInterno() {
+  let div = document.createElement("div");
+  div.className = "uk-grid-collapse uk-child-width-1-2@s uk-flex-middle";
+  div.setAttribute("uk-grid", "");
+  return div;
+}
+
+function crearDivImage() {
+  let div = document.createElement("div");
+  div.id = "iamge_modal";
+  div.className = "uk-background-conver";
+  div.setAttribute("uk-height-viewport", "");
+  return div;
+}
+
+function crearDivDescription() {
+  let div = document.createElement("div");
+  div.className = "uk-padding-large";
+  let h1 = document.createElement("h1");
+  h1.id = "title_modal";
+  let p = document.createElement("h1");
+  p.id = "description_modal";
+  div.appendChild(h1);
+  div.appendChild(p);
+  return div;
+}
