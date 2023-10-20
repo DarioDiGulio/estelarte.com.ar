3016643ea36b3d1070f629510ab4b4943f0b396f
Dario Di Gulio <dario.digulio@grupoesfera.com.ar>
2023-10-19 23:44:33 -0300

    Argrega comportamiento del catalogo

---
+++ src/js/comportamientoCarrito.js
@@ -0,0 +1,32 @@
+function renderiarProductos() {
+    const carrito = JSON.parse(localStorage.getItem('carrito'))
+    const items = document.getElementById('items')
+    if (carrito.length === 0) {
+        renderEmptyChart(items)
+        return
+    }
+    carrito.forEach(item => {
+        const producto = dataProductos.find(product => product.id === item.id)
+        items.innerHTML += `
+            <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+                <div class="uk-card-media-left uk-cover-container">
+                    <img src="${producto.src}" alt="" uk-cover>
+                    <canvas width="600" height="400"></canvas>
+                </div>
+                <div>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title">${producto.title}</h3>
+                        <p>${producto.description}</p>
+                        <p>${producto.price}</p>
+                    </div>
+                </div>
+            </div>
+        `
+    })
+}
+
+function renderEmptyChart(items) {
+    items.innerText = 'Vacío'
+}
+
+renderiarProductos()
--- src/js/storage.js
+++ src/js/storage.js
@@ -1,4 +1,91 @@
-localStorage.setItem('carrito', JSON.stringify([]))
+sessionStorage.setItem('carrito', JSON.stringify([]))
+
+const dataProductos = [
+    {
+        id: "Despojadores de biju chico",
+        src: "../../imagenes/catalogo/despojadorChico.jpg",
+        title: "Despojadores de biju",
+        description: "Chico",
+        price: "2000"
+    },
+    {
+        id: "Despojadores de biju grande",
+        src: "../../imagenes/catalogo/despojadorGrande.jpg",
+        title: "Despojadores de biju",
+        description: "Grande",
+        price: "2500"
+    },
+    {
+        id: "Manito de Fátima",
+        src: "../../imagenes/catalogo/portaSaumerios.jpg",
+        title: "Manito de Fátima",
+        description: "Porta saumerios",
+        price: "1500"
+    },
+    {
+        id: "Posa vaso",
+        src: "../../imagenes/catalogo/posaVaso.jpg",
+        title: "Posa vaso",
+        description: "Efecto mármol",
+        price: "2000"
+    },
+    {
+        id: "Bandejas de Terrazo oval",
+        src: "../../imagenes/catalogo/bandejaDeTerrazo.jpg",
+        title: "Bandejas de Terrazo oval",
+        description: "Varios colores - 18cm",
+        price: "4500"
+    },
+    {
+        id: "Bandejas de Terrazo grande",
+        src: "../../imagenes/catalogo/bandejaDeTerrazoGrande.jpg",
+        title: "Bandejas de Terrazo",
+        description: "Para mesa - 19cm",
+        price: "8000"
+    },
+    {
+        id: "Bandejas de Terrazo con chispas",
+        src: "../../imagenes/catalogo/bandejaDeTerrazoConChispas.jpg",
+        title: "Bandejas de Terrazo",
+        description: "Con chispas de terrazo - 18cm",
+        price: "4700"
+    },
+    {
+        id: "Porta Vela",
+        src: "../../imagenes/catalogo/portaVela.jpg",
+        title: "Porta Vela",
+        description: "Varios colores",
+        price: "1700"
+    },
+    {
+        id: "SET de Baño Completo",
+        src: "../../imagenes/catalogo/banioCompleto.jpg",
+        title: "SET de Baño Completo",
+        description: "Bandeja - Porta vela - Dispenser de jabón líquido",
+        price: "10000"
+    },
+    {
+        id: "Combo de Baño",
+        src: "../../imagenes/catalogo/comboBanio.jpg",
+        title: "Combo de Baño",
+        description: "Bandeja - Dispenser de jabón líquido",
+        price: "7000"
+    },
+    {
+        id: "Dispenser de jabón líquido",
+        src: "../../imagenes/catalogo/dispenserJabon.jpg",
+        title: "Dispenser de jabón líquido",
+        description: "Terrazo",
+        price: "5000"
+    },
+    {
+        id: "Mesa pua",
+        src: "../../imagenes/catalogo/mesaPua.jpg",
+        title: "Mesa pua",
+        description: "70 x 50cm - Resina epoxi - Color mármol",
+        price: "29000"
+    },
+]
 
 function aumentarCantidad(producto) {
     producto.cantidad = parseInt(producto.cantidad) + 1
@@ -9,19 +96,19 @@ function restarCantidad(producto) {
 }
 
 function eliminarProducto(index) {
-    const carrito = JSON.parse(localStorage.getItem('carrito'))
+    const carrito = JSON.parse(sessionStorage.getItem('carrito'))
     carrito.splice(index, 1)
     return carrito
 }
 
 function existeProductoEnCarrito(id) {
-    const carrito = JSON.parse(localStorage.getItem('carrito'))
+    const carrito = JSON.parse(sessionStorage.getItem('carrito'))
     const filter = carrito.filter(producto => producto.id === id)
     return filter.length !== 0
 }
 
 function agregarProducto(id) {
-    const carrito = JSON.parse(localStorage.getItem('carrito'))
+    const carrito = JSON.parse(sessionStorage.getItem('carrito'))
     if (carrito.length === 0 || !existeProductoEnCarrito(id)) {
         carrito.push({id, 'cantidad': 1})
     } else {
@@ -29,11 +116,11 @@ function agregarProducto(id) {
             if (producto.id === id) aumentarCantidad(producto);
         })
     }
-    localStorage.setItem('carrito', JSON.stringify(carrito))
+    sessionStorage.setItem('carrito', JSON.stringify(carrito))
 }
 
 function descontarProducto(id) {
-    let carrito = JSON.parse(localStorage.getItem('carrito'))
+    let carrito = JSON.parse(sessionStorage.getItem('carrito'))
     if (carrito.length === 0) {
         return
     } else {
@@ -42,5 +129,5 @@ function descontarProducto(id) {
             if (producto.cantidad <= 0) carrito = eliminarProducto(index);
         })
     }
-    localStorage.setItem('carrito', JSON.stringify(carrito))
+    sessionStorage.setItem('carrito', JSON.stringify(carrito))
 }
